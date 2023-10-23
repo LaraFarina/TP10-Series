c35527ae038bb6ba46135e963425ef2de17da2bc
LARA MICAELA FARINA <47871657@ort>
2023-10-23 10:35:20 -0300

    Avances js

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,1 +1,50 @@
 ﻿// Utilizar el archivo site.js para definir las funciones de Javascript que van a cargar la información mediante Ajax.  
+function MostarTemporadas(IdS){
+    $.ajax(
+        {
+            type: 'POST',
+            datatype: 'JSON',
+            url: '/Home/VerTemporadas',
+            data: {IdSerie : IdS},
+            success: 
+            function (response){   
+                $("#TituloTemporada").html(response.List)
+            }
+        }
+
+    )
+        
+    }
+
+    function MostarActores(IdA){
+        $.ajax(
+            {
+                type: 'POST',
+                datatype: 'JSON',
+                url: '/Home/VerActores',
+                data: {IdSerie : IdS},
+                success: 
+                function (response){
+                    $("#TituloTemporada").html(response.List)
+                }
+            }
+    
+        )
+            
+        }
+        function MostarTemporadas(IdS){
+            $.ajax(
+                {
+                    type: 'POST',
+                    datatype: 'JSON',
+                    url: '/Home/VerTemporadas',
+                    data: {IdSerie : IdS},
+                    success: 
+                    function (response){
+                        $("#TituloTemporada").html(response.List)
+                    }
+                }
+        
+            )
+                
+            }
